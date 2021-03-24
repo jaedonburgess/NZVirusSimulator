dca2e193f0ef0f1a58d2ec75b3b4e98de2dd552b
Jaedon Burgess <jbapps2005@gmail.com>
2021-03-25 10:36:42 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NZVirusSimulator
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
